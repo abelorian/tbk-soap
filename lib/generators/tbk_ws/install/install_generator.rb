7a118102718d7bf0a57392464a53b6c442cba9c7
Abel O'Rian <abel.orian@gmail.com>
2017-10-17 17:23:36 -0300

    Fix: templates paths

--- lib/generators/tbk_ws/install/install_generator.rb
+++ lib/generators/tbk_ws/install/install_generator.rb
@@ -1,7 +1,7 @@
 module TbkWs
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path('.', __FILE__)
+      source_root File.expand_path('../templates', __FILE__)
 
       def generate_config_file
         copy_file 'tbk_ws_config.rb', 'config/initializers/tbk_ws_config.rb'
