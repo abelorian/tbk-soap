0fad4cb80ffeff1c464a132267a73ec3df80c0ac
Abel O'Rian <abel.orian@gmail.com>
2017-10-19 17:36:05 -0300

    Fix: improve loggers

--- lib/transbank/client.rb
+++ lib/transbank/client.rb
@@ -11,20 +11,21 @@ module Transbank
     end
 
     def make_request action, message_data
-      show_log(action)
+      show_log("action", action)
+      show_log("message_data", message_data)
       req = @client.build_request(action.to_sym, message: message_data)
-      show_log(message_data)
+      show_log("xml_request", req)
       p message_data
       signed_xml = @document.sign_xml(req)
       begin
         response = @client.call(action.to_sym) do
           xml signed_xml.to_xml(:save_with => 0)
         end
-      show_log(response)
+        show_log("response",response)
 
       rescue Exception, RuntimeError => e
         p "---------------- error"
-        p e
+        p e.inspect
         return "INVALID"
         raise
       end
@@ -32,9 +33,9 @@ module Transbank
       response
     end
 
-    def show_log message
-      p "----- Transbank Webpay log: #{message}"
+    def show_log key, message
+      p "----- Transbank Webpay log: #{key}"
+      p message
     end
-
   end
 end
